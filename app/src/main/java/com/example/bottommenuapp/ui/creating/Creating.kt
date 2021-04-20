e76db9bf9a5455efa4c1062280ef062de21bd05c
Полина <gossipgirl2709@mail.ru>
2021-04-20 20:48:37 +0300

    Добавила радиобаттн "Любое" для времени + настроила видимость поля для ввода колва участников по switch + белый цвет

--- app/src/main/java/com/example/bottommenuapp/ui/creating/Creating.kt
+++ app/src/main/java/com/example/bottommenuapp/ui/creating/Creating.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.Switch
 import com.example.bottommenuapp.R
+import com.google.android.material.textfield.TextInputLayout
 
 class Creating : Fragment() {
 
@@ -16,17 +19,35 @@ class Creating : Fragment() {
 
     private lateinit var viewModel: CreatingViewModel
 
+    // Отображает creating_fragment.xml
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.creating_fragment, container, false)
+
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(CreatingViewModel::class.java)
         // TODO: Use the ViewModel
+
+
+        // возможно, нужно очищать поле или при мапинге в бд смотреть на состояние свитч
+        view?.findViewById<Switch>(R.id.switchEventParticipantsCount)?.setOnCheckedChangeListener { _, isChecked ->
+            view?.findViewById<TextInputLayout>(R.id.textInputLayoutEventParticipantsCount)?.visibility = if (isChecked) View.VISIBLE
+            else
+                View.INVISIBLE
+    }
+
+//    fun switchParticipantsCountVisibility (view: View) {
+//        val switchParticipantCount = view.findViewById<Switch>(R.id.switchEventParticipantsCount)
+//        switchParticipantCount.setOnCheckedChangeListener { _, isChecked ->
+//            view.findViewById<TextInputLayout>(R.id.textInputLayoutEventParticipantsCount).visibility = if (isChecked) View.VISIBLE
+//            else View.GONE
+//        }
     }
 
 }
